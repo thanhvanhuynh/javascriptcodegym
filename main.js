3139cbaa656911fbf22a99ed146c4060703b5512
thanhvanhuynh <nguhanh111@gmail.com>
2022-11-12 21:39:29 +0700

    ngay thang nam

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-let widths = prompt("Nhập chiều rộng");
-let heights = prompt("Nhập chiều dài");
-let widthparse = parseInt(widths);
-document.write("Chieu rong: " + widthparse + "m");
-let heightparse = parseInt(heights);
-document.write("</br>");
-document.write("Chieu dai : " + heightparse + "m");
-document.write("</br>");
-let total = widthparse * heightparse;
-document.write("Dien tich hinh chu nhat : " + total + " m2");
+let year = prompt("Nhập năm :");
+let month = prompt("Nhập tháng :");
+let date = prompt("Nhập ngày :");
+document.write(
+  "Giá trị được nhận ngày tháng năm " + date + "/" + month + "/" + year
+);
