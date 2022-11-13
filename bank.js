bc3bf3e80dad7ac8c28d5f36feeb45218730625f
thanhvanhuynh <nguhanh111@gmail.com>
2022-11-13 11:52:43 +0700

    letter

---
+++ bank.js
@@ -0,0 +1,15 @@
+// let A = Number();
+// let B = Number();
+// A = prompt("Số tiền cần vay ");
+// B = prompt("Số năm vay ");
+
+// document.write("Số tiền vay : " + A + " đồng");
+// document.write("</br>");
+
+// document.write("Thời gian vay : " + B + " năm");
+// document.write("</br>");
+// document.write("Lãi vay : 7%/năm");
+// document.write("</br>");
+// let C = A + B * ((A * 7) / 100);
+
+// document.write("SỐ TIỀN PHẢI TRẢ : " + C + " ĐỒNG");
---
+++ circle.js
@@ -0,0 +1,8 @@
+let R = prompt("Bán kính đường tròn");
+let pi = 3.14;
+let C = 2 * R * pi;
+let S = R * R * 3.14;
+document.write("Chu vi đường tròn   :" + C);
+document.write("</br>");
+
+document.write("Diện tích đường tròn    :" + S);
