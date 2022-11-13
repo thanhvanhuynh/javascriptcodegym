5c757689fa2827ae967a397a58d26f50b65d5381
thanhvanhuynh <nguhanh111@gmail.com>
2022-11-13 11:54:04 +0700

    letter

--- dates.js
+++
@@ -1,6 +0,0 @@
-let year = prompt("Nhập năm :");
-let month = prompt("Nhập tháng :");
-let date = prompt("Nhập ngày :");
-document.write(
-  "Giá trị được nhận ngày tháng năm " + date + "/" + month + "/" + year
-);
